c9d532540b5f1ee36b55cfdce9a5cc3056ebad81
Camilo Jimenez <camiloko1989@gmail.com>
2022-09-27 22:17:41 -0700

    Add methods to the carpet area calculator

---
+++ DouglasName/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DouglasName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Lines in each letter
+            for(int i = 0; i < 8; i++)
+            {   
+                if(i == 0 || i == 7)
+                {
+                    Console.WriteLine("DDDDD");
+                } else if(i == 1 || i == 6)
+                {
+                    Console.WriteLine("D    D");
+                } else if (i == 2 || i == 5)
+                {
+                    Console.WriteLine("D     D");
+                } else if (i == 3 || i == 4)
+                {
+                    Console.WriteLine("D      D");
+                } 
+            }
+            ReadKey();
+            //2. Number of letters
+        }
+    }
+}
